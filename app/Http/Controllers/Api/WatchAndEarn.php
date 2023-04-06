36def5bc4e47d936dd7e48edf7d53e42ccc7dbc8
codexindia <sudiptobain8@gmail.com>
2023-04-06 21:23:49 +0530

    bug

--- app/Http/Controllers/Api/WatchAndEarn.php
+++ app/Http/Controllers/Api/WatchAndEarn.php
@@ -55,6 +55,7 @@ class WatchAndEarn extends Controller
         $result = (new WalletManage)->AddLog($user_id,$amount,$description,$status);
        return response()->json([
         'status' => true,
+        'amount' => $amount
         'message' => 'Reward Has Been Added In Your Wallet'
        ]);
     }
