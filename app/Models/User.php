aa5755f33b902d597e4b1d6fb6e7be65277b81c3
codexindia <sudiptobain8@gmail.com>
2023-04-09 15:17:56 +0530

    profile bug

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Storage;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -45,4 +46,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function getProfilePicAttribute($value)
+    {
+        return asset(Storage::url($value));
+
+    }
 }
