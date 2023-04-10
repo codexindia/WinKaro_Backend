0765de203dc1af3a21cab0fa347c31eba74d93a1
codexindia <sudiptobain8@gmail.com>
2023-04-10 20:28:53 +0530

    changes

--- app/Http/Controllers/Api/ProfileController.php
+++ app/Http/Controllers/Api/ProfileController.php
@@ -6,16 +6,21 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Storage;
-
+use App\Models\CompleteTask;
 
 class ProfileController extends Controller
 {
    public function getuser(Request $request)
    {
       $user = User::find($request->user()->id);
+      $data['complete_tasks'] = CompleteTask::where([
+         'status' => 'success',
+         'user_id'  => $request->user()->id,
+      ])->count();
       $data['alert_count'] = $user->unreadNotifications->count();
       return response()->json([
          'status' => true,
+         'complete_tasks' => $data['complete_tasks'],
          'unread_alert' => $data['alert_count'],
          'data' => $request->user(),
          'message' => 'user retrieve successfully',
