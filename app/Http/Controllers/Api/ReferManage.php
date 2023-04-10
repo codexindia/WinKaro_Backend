4f936e2d221c1bbd35b4c84109014c0bb2c905d6
codexindia <sudiptobain8@gmail.com>
2023-04-10 15:48:39 +0530

    bugsdsd

--- app/Http/Controllers/Api/ReferManage.php
+++ app/Http/Controllers/Api/ReferManage.php
@@ -5,24 +5,24 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\ReferHistory;
+
 class ReferManage extends Controller
 {
     public function get_history(Request $request)
     {
-    $main = ReferHistory::where('refer_by_user_id',$request->user()->id)->get(['refer_by_user_id','referred_user_id','status','created_at']);
-    $data = [];
-   
-   foreach($main as $item)
-   {
-    
-  //  $item->GetName->makeHidden('updated_at','created_at','phone','email','balance','refer_code','referred_by');
-  $item->GetName->makeHidden('updated_at','created_at','phone','email','balance','refer_code','referred_by');
-  $data = $item;
-   }
-    return response()->json([
-        'status' => true,
-        'data' => $data,
-        'message' => 'Refer History Fetched SuccessFully',
-    ]);
+        $main = ReferHistory::where('refer_by_user_id', $request->user()->id)->get(['refer_by_user_id', 'referred_user_id', 'status', 'created_at']);
+        $data = [];
+
+        foreach ($main as $item) {
+
+            //  $item->GetName->makeHidden('updated_at','created_at','phone','email','balance','refer_code','referred_by');
+            $item->GetName->makeHidden('updated_at', 'created_at', 'phone', 'email', 'balance', 'refer_code', 'referred_by');
+            $data = [$item];
+        }
+        return response()->json([
+            'status' => true,
+            'data' => $data,
+            'message' => 'Refer History Fetched SuccessFully',
+        ]);
     }
 }
