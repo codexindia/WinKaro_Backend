fa301c94816c387a3502f4bccc9a37ff20588826
codexindia <sudiptobain8@gmail.com>
2023-05-20 14:14:29 +0530

    upload memthod

--- app/Http/Controllers/Api/TaskManage.php
+++ app/Http/Controllers/Api/TaskManage.php
@@ -18,9 +18,9 @@ class TaskManage extends Controller
         $user = $request->user();
         $type = $request->type;
         $taskdata = AllTasks::where([
-            'type'=> $type,
+            'type' => $type,
             'status' => 'active'
-            ])->orderBy('id', 'desc')->get();
+        ])->orderBy('id', 'desc')->get();
         $data = [];
         $i = 0;
         foreach ($taskdata as $collection) {
@@ -30,7 +30,7 @@ class TaskManage extends Controller
                 'user_id' => $request->user()->id,
             ])->latest()->first();
             if ($check != null) {
-                
+
                 $data[] = [
                     'data' => $collection,
                     'status' => $check->status,
@@ -39,10 +39,9 @@ class TaskManage extends Controller
             } else {
                 $data[] = [
                     'data' => $collection,
-                    
+
                 ];
             }
-
         }
         return response()->json([
             'status' => true,
@@ -77,65 +76,61 @@ class TaskManage extends Controller
 
     public function submit_task_v2(Request $request)
     {
-       
+        $request->validate([
+            'task_id' => 'required|numeric|exists:all_tasks,id',
+
+        ]);
 
         try {
             $chunk = new Chunk([
                 'name' => 'video', // same as    $_FILES['video']
                 'chunk_size' => 1000000, // must be equal to the value specified on the client side
-        
+
                 // Driver
                 'driver' => 'local', // [local, ftp]
-        
+
                 // Local driver details
                 'local_driver' => [
                     'path' => public_path('/storage/users/proof/'), // where to upload the final file
                     'tmp_path' => public_path('/storage/users/proof/temp/'), // where to store the temp chunks
                 ],
-        
+
                 // File details
                 'file_name' => Chunk::RANDOM_FILE_NAME,
                 'file_extension' => Chunk::ORIGINAL_FILE_EXTENSION,
-        
+
                 // File validation
                 'validation' => ['extension:mp4,avi,3gp'],
             ]);
-        
+
             $chunk->validate()->store();
-        
+
             if ($chunk->isLast()) {
-                $proof_src = '/public/users/proof/'.$chunk->createFileName();
-                $get_task = AllTasks::findOrFail(18);
+                $proof_src = '/public/users/proof/' . $chunk->createFileName();
+                $get_task = AllTasks::findOrFail($request->task_id);
                 CompleteTask::create([
-                    'user_id' => 63,
+                    'user_id' => $request->user()->id,
                     'task_id' => $get_task->id,
                     'type' => $get_task->type,
                     'reward_coin' => $get_task->reward_coin,
                     'proof_src' => $proof_src,
                     'status' => 'processing',
                 ]);
-               
+
                 return response()->json([
                     'status' => true,
                     'message' => 'Task Submitted SuccessFully',
                 ]);
-                
-                
             } else {
                 $chunk->response()->json([
                     'progress' => $chunk->getProgress()
                 ]);
             }
-        
         } catch (ValidationException $e) {
             $e->response(422)->json([
                 'message' => $e->getMessage(),
                 'data' => $e->getErrors(),
             ]);
-        } 
-
-
-      
-       
+        }
     }
 }
