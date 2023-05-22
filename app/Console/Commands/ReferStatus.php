651d4f556e8d157ec5eaedf36f1195e247f43aad
codexindia <sudiptobain8@gmail.com>
2023-05-23 03:11:40 +0530

    bug fix

--- app/Console/Commands/ReferStatus.php
+++ app/Console/Commands/ReferStatus.php
@@ -28,7 +28,7 @@ class ReferStatus extends Command
      */
     public function handle(): void
     {
-        $get_pendings = ReferHistory::where(['status' => 'pending'])->limit(10)->get();
+        $get_pendings = ReferHistory::where(['status' => 'pending'])->get();
         //loop for origin users only
         foreach ($get_pendings as $get) {
             $get_completed_task  = CompleteTask::where([
