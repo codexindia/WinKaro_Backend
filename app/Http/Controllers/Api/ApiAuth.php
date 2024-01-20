7e82520dc3f725ce32010a662cff14092b5ce742
codexindia <sudiptobain8@gmail.com>
2024-01-20 16:18:10 +0530

    s

--- app/Http/Controllers/Api/ApiAuth.php
+++ app/Http/Controllers/Api/ApiAuth.php
@@ -21,7 +21,7 @@ class ApiAuth extends Controller
     public function register(Request $request)
     {
         $request->validate([
-            'name' => 'required|regex:/^[a-zA-Z]+$/u|max:255',
+            'name' => 'required|alpha_dash|max:255',
             'email' => 'required|unique:users,email',
             'phone' => 'required|unique:users,phone',
             'device_id' => 'required',
